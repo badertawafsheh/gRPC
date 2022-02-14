10a8a91a32f3b9b9aac51054fb6ad6552e62e08b
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-02-14 15:40:34 +0200

    Last Changes

--- gRPC_Client/Program.cs
+++ gRPC_Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Net.Client;
+using gRPC_Client_Server;
 using System;
 using System.Threading.Tasks;
 
@@ -8,17 +9,22 @@ namespace gRPC_Client
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("press any key to cont. ");
-            Console.ReadLine();
-            var input = new HelloRequest
-            {
-                Name = "BADER"
-            };
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(input);
-            Console.WriteLine($"Greetings : {reply.Message}");
-            Console.WriteLine("press any key to exit");
+            //Console.WriteLine("press any key to cont. ");
+            //Console.ReadLine();
+            //var input = new HelloRequest
+            //{
+            //    Name = "BADER"
+            //};
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine($"Greetings : {reply.Message}");
+            //Console.WriteLine("press any key to exit");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var CustomerClient = new Customer.CustomerClient(channel);
+            var clientRequested = new CustomerLookUpModel { UserId = 2 };
+            var customer = await CustomerClient.GetCustomerInfoAsync(clientRequested);
+            Console.WriteLine($"{customer.FirstName} {customer.LastName}");
             Console.ReadLine();
         }
     }
