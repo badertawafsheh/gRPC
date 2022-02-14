658caf956fec7f4d154600adb8a317a203499121
Bader Tawafsheh <68567544+badertawafsheh@users.noreply.github.com>
2022-02-14 10:35:32 +0200

    Add project files.

---
+++ gRPC_Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPC_Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("press any key to cont. ");
+            Console.ReadLine();
+            var input = new HelloRequest
+            {
+                Name = "BADER"
+            };
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(input);
+            Console.WriteLine($"Greetings : {reply.Message}");
+            Console.WriteLine("press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
